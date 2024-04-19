8f6260524fd34479cb139baff2be4f3b9d6c3187
inrage <inrag3x@gmail.com>
2024-04-19 21:50:04 +0300

    add: level prefab and adaptive ui

---
+++ Assets/SafeAreaContainer.cs
@@ -0,0 +1,30 @@
+using System;
+using UnityEngine;
+
+[RequireComponent(typeof(RectTransform))]
+public class SafeAreaContainer : MonoBehaviour
+{
+    private RectTransform _rectTransform;
+
+    private void OnValidate()
+        => _rectTransform ??= GetComponent<RectTransform>();
+    
+    private void Awake()
+        => UpdateArea();
+
+    private void UpdateArea()
+    {
+        var safeArea = Screen.safeArea;
+
+        var anchorMin = safeArea.position;
+        var anchorMax = safeArea.position + safeArea.size;
+
+        anchorMin.x /= Screen.width;
+        anchorMin.y /= Screen.height;
+        anchorMax.x /= Screen.width;
+        anchorMax.y /= Screen.height;
+
+        _rectTransform.anchorMin = anchorMin;
+        _rectTransform.anchorMax = anchorMax;
+    }
+}
